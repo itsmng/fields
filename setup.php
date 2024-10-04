1087f5d19e0743e5e7ab383ddbba95bdd2e697ba
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2024-10-04 10:45:56 +0200

    feat: update field name

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define ('PLUGIN_FIELDS_VERSION', '1.14');
+define ('PLUGIN_FIELDS_VERSION', '1.15');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_FIELDS_MIN_GLPI", "9.5");
