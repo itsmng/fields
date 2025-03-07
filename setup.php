92d525ed523f54fe0a26fc4f94622335508fe447
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2025-03-07 10:28:56 +0100

    feat: bump version

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define ('PLUGIN_FIELDS_VERSION', '2.0.0');
+define ('PLUGIN_FIELDS_VERSION', '2.0.1');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_FIELDS_MIN_GLPI", "9.5");
