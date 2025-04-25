ce1953f6acd5f3fa1a2a9cffa7978256c98cb7f0
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2025-04-25 09:27:01 +0200

    feat: add genericobject integration

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define('PLUGIN_FIELDS_VERSION', '1.15');
+define('PLUGIN_FIELDS_VERSION', '1.16');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_FIELDS_MIN_GLPI", "9.5");
