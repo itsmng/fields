f337b612890f972c26b9f4b656a24612fde90f24
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2025-05-21 14:53:03 +0200

    fix: update name of label translations when deleting container

--- inc/container.class.php
+++ inc/container.class.php
@@ -568,8 +568,8 @@ class PluginFieldsContainer extends CommonDBTM {
          //delete label translations
          $translation_obj = new PluginFieldsLabelTranslation();
          $translation_obj->deleteByCriteria([
-            'plugin_fields_itemtype' => self::getType(),
-            'plugin_fields_items_id' => $this->fields['id']
+            'itemtype' => self::getType(),
+            'items_id' => $this->fields['id']
          ]);
 
          //delete table
