f34ebc69e59c9fefbc197c58d1c77712c47e3aa5
théodore clément <213218350+thclemnt@users.noreply.github.com>
2026-04-17 10:03:43 +0200

    build: bump version

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define('PLUGIN_FIELDS_VERSION', '1.18.1');
+define('PLUGIN_FIELDS_VERSION', '1.18.3');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_FIELDS_MIN_GLPI", "9.5");
